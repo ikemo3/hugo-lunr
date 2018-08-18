9afd75b4c626899965c56b3af867facba14fd216
Hideki Ikemoto <ikemo333@gmail.com>
2018-08-18 14:13:17 +0900

    add search page.

--- assets/template/pageindex.js
+++ assets/template/pageindex.js
@@ -1,18 +1,7 @@
-var sitePageList = [{{ range $i, $e := .Site.Pages }}{{ if $i }}, {{ end }}
+var HugoIndexes = [{{ range $i, $e := .Site.Pages }}{{ if $i }}, {{ end }}
   {
     "title": {{ .Title | jsonify }},
     "url": "{{ .Permalink }}",
     "content": {{ .Plain | jsonify }}
   }{{ end }}
 ]
-
-const HugoIndex = Lunr(function () {
-    this.use(Lunr.jp);
-    this.ref('url');
-    this.field('title');
-    this.field('content');
-
-    sitePageList.forEach(function(page) {
-        this.add(page);
-    }, this);
-});
